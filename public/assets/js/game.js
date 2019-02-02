285f1f94e925aa6b1fae8a02efbcd62ea41950b1
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-02-02 12:42:09 -0700

    game.js reading from the database and printing to quiz.html, need to fix printing of all choices

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -6,34 +6,38 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  // saveExample: function(example) {
+  //   return $.ajax({
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     type: "POST",
+  //     url: "api/examples",
+  //     data: JSON.stringify(example)
+  //   });
+  // },
+  getQuestions: function (cb) {
     return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
+      url: "api/questions",
       type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
+    }).then(function (data) {
+      //  console.log(data);
+      cb(data);
     });
   }
+  //,
+  // deleteExample: function(id) {
+  //   return $.ajax({
+  //     url: "api/examples/" + id,
+  //     type: "DELETE"
+  //   });
+  // }
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -61,85 +65,85 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
+// var handleFormSubmit = function(event) {
+//   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
+//   var example = {
+//     text: $exampleText.val().trim(),
+//     description: $exampleDescription.val().trim()
+//   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
+//   if (!(example.text && example.description)) {
+//     alert("You must enter an example text and description!");
+//     return;
+//   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
+//   API.saveExample(example).then(function() {
+//     refreshExamples();
+//   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
+//   $exampleText.val("");
+//   $exampleDescription.val("");
+// };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+// var handleDeleteBtnClick = function() {
+//   var idToDelete = $(this)
+//     .parent()
+//     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
+//   API.deleteExample(idToDelete).then(function() {
+//     refreshExamples();
+//   });
+// };
 
 // QUIZ AREA
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
 
 var card = $("#quiz-area");
 var countStartNumber = 30;
 
 // Question set
-var questions = [{
-  "category": "Moon",
-  "question": "How far away is the moon?",
-  "answers": ["238,900 miles", "310,000 miles", "19,586 miles", "524,356 miles"],
-  "correctAnswer": "238,900",
-  "image": "assets/images/moonRotation.gif"
-}, {
-  "category": "Moon",
-  "question": "What is the orbital period of the moon?",
-  "answers": ["2 months", "27 days", "2 weeks", "none"],
-  "correctAnswer": "27 days",
-  "image": "assets/images/moonOrbit.gif"
-}, {
-  "category": "Constellation",
-  "question": "What star will lead you north?",
-  "answers": ["Santa Claus", "Celine Dion", "Polaris", "Little Dipper"],
-  "correctAnswer": "Polaris",
-  "image": "assets/images/polaris.gif"
-}, {
-  "category": "Constellation",
-  "question": "How many stars make up the belt of Orion?",
-  "answers": ["7", "5", "19", "3"],
-  "correctAnswer": "3",
-  "image": "https://thumbs.gfycat.com/IllinformedShockedBlesbok-size_restricted.gif"
-}, {
-  "category": "Mars",
-  "question": "When did the first robot land on Mars?",
-  "answers": ["1997", "1900", "2000", "2002"],
-  "correctAnswer": "1997",
-  "image": "assets/images/sojourner.gif"
-}, {
-  "category": "Mars",
-  "question": "What color is Mars?",
-  "answers": ["Blue", "Red", "Green", "Hazy"],
-  "correctAnswer": "Red",
-  "image": "https://media.giphy.com/media/cmzkzpxPS3xl8hRVJm/giphy.gif"
-}];
+var questions = [
+  //{
+  //   "category": "Moon",
+  //   "question": "How far away is the moon?",
+  //   "answers": ["238,900 miles", "310,000 miles", "19,586 miles", "524,356 miles"],
+  //   "correctAnswer": "238,900",
+  //   "image": "assets/images/moonRotation.gif"
+  // }, {
+  //   "category": "Moon",
+  //   "question": "What is the orbital period of the moon?",
+  //   "answers": ["2 months", "27 days", "2 weeks", "none"],
+  //   "correctAnswer": "27 days",
+  //   "image": "assets/images/moonOrbit.gif"
+  // }, {
+  //   "category": "Constellation",
+  //   "question": "What star will lead you north?",
+  //   "answers": ["Santa Claus", "Celine Dion", "Polaris", "Big Dipper"],
+  //   "correctAnswer": "Polaris",
+  //   "image": "assets/images/polaris.gif"
+  // }, {
+  //   "category": "Constellation",
+  //   "question": "How many stars make up the belt of Orion?",
+  //   "answers": ["7", "5", "19", "3"],
+  //   "correctAnswer": "3",
+  //   "image": "https://thumbs.gfycat.com/IllinformedShockedBlesbok-size_restricted.gif"
+  // }, {
+  //   "category": "Mars",
+  //   "question": "When did the first robot land on Mars?",
+  //   "answers": ["1997", "1900", "2000", "2002"],
+  //   "correctAnswer": "1997",
+  //   "image": "assets/images/sojourner.gif"
+  // }, {
+  //   "category": "Mars",
+  //   "question": "What color is Mars?",
+  //   "answers": ["Blue", "Red", "Green", "Hazy"],
+  //   "correctAnswer": "Red",
+  //   "image": "https://media.giphy.com/media/cmzkzpxPS3xl8hRVJm/giphy.gif"
+  // }
+];
 
 // Variable to hold our setInterval
 var timer;
@@ -151,7 +155,7 @@ var game = {
   correct: 0,
   incorrect: 0,
 
-  countdown: function() {
+  countdown: function () {
     game.counter--;
     $("#counter-number").text(game.counter);
     if (game.counter === 0) {
@@ -160,26 +164,37 @@ var game = {
     }
   },
 
-  loadQuestion: function() {
-    timer = setInterval(game.countdown, 1000);
+  loadQuestion: function () {
+    API.getQuestions(function (data) {
+      questions = data;
+      console.log(questions);
+      timer = setInterval(game.countdown, 1000);
 
-    card.html("<h2>The category is: " + questions[this.currentQuestion].category + "</h2>" +
-     "<h2>" + questions[this.currentQuestion].question + "</h2>");
+      // console.log(currentQuestion);
 
-    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-      card.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
-      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
-    }
+      
+
+      for (var i = 0; i < questions.length; i++) {
+        card.html("<h2>The category is: " + questions[i].category + "</h2>" +
+        "<h2>" + questions[i].question + "</h2>");
+
+        card.append("<button class='answer-button' id='button' data-name='" + questions[i].choices[0]
+          + "'>" + questions[i].choices[0] + "</button>");
+      }
+
+    });
+
+
   },
 
-  nextQuestion: function() {
+  nextQuestion: function () {
     game.counter = countStartNumber;
     $("#counter-number").text(game.counter);
     game.currentQuestion++;
     game.loadQuestion();
   },
 
-  timeUp: function() {
+  timeUp: function () {
     clearInterval(timer);
 
     $("#counter-number").html(game.counter);
@@ -195,7 +210,7 @@ var game = {
     }
   },
 
-  results: function() {
+  results: function () {
     clearInterval(timer);
 
     card.html("<h2>All done, heres how you did!</h2>");
@@ -208,7 +223,7 @@ var game = {
     card.append("<br><button id='start-over'>Start Over?</button>");
   },
 
-  clicked: function(e) {
+  clicked: function (e) {
     clearInterval(timer);
     if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
       this.answeredCorrectly();
@@ -217,7 +232,7 @@ var game = {
     }
   },
 
-  answeredIncorrectly: function() {
+  answeredIncorrectly: function () {
     game.incorrect++;
 
     clearInterval(timer);
@@ -235,7 +250,7 @@ var game = {
     }
   },
 
-  answeredCorrectly: function() {
+  answeredCorrectly: function () {
     clearInterval(timer);
 
     game.correct++;
@@ -250,7 +265,7 @@ var game = {
     }
   },
 
-  reset: function() {
+  reset: function () {
     this.currentQuestion = 0;
     this.counter = countStartNumber;
     this.correct = 0;
@@ -261,15 +276,15 @@ var game = {
 
 // CLICK EVENTS
 
-$(document).on("click", "#start-over", function() {
+$(document).on("click", "#start-over", function () {
   game.reset();
 });
 
-$(document).on("click", ".answer-button", function(e) {
+$(document).on("click", ".answer-button", function (e) {
   game.clicked(e);
 });
 
-$(document).on("click", "#start", function() {
+$(document).on("click", "#start", function () {
   $("#sub-wrapper").prepend(
     "<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>"
   );
