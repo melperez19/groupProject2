436e69da83d22de07baa5862a3347ede793a01c5
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-02-02 14:48:27 -0700

    added more values to seeds and schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,28 @@ USE spaceDB;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+DESCRIBE user_question_answers;
 
 CREATE TABLE user (
-    user_id INTEGER(10) AUTO_INCREMENT,    
+    -- user_id INTEGER(10) AUTO_INCREMENT,    
     regtime DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
-    user_name VARCHAR(45) NOT NULL,
-   PRIMARY KEY (user_id)
+    user_name VARCHAR(45) NOT NULL
+   -- PRIMARY KEY (user_id)
 );
 
 CREATE TABLE questions (
-   question_id INTEGER(10) AUTO_INCREMENT,
+   -- question_id INTEGER(10) AUTO_INCREMENT,
    category VARCHAR(255) NOT NULL,
    question VARCHAR(255) NOT NULL,
    choices VARCHAR(255) NOT NULL,
    correctAnswer INTEGER NOT NULL,
-   PRIMARY KEY (question_id)
+   image TEXT NOT NULL
+   -- PRIMARY KEY (question_id)
  );
 
 CREATE TABLE user_question_answer (
-    user_id INTEGER(10) PRIMARY KEY,    
-    question_id INTEGER(10),
+    -- user_id INTEGER(10) PRIMARY KEY,    
+    -- question_id INTEGER(10),
    users_choice INTEGER,
    is_correct BOOLEAN DEFAULT FALSE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,12 +3,14 @@ INSERT INTO user (user_name) values ('Mark');
 INSERT INTO user (user_name) values ('Lewis');
 INSERT INTO user (user_name) values ('Andre');
 
-INSERT INTO questions (category, question, choices, correctAnswer) values ('Moon', "How are far away is the moon?", "a, b, c, d", 0);
-INSERT INTO questions (category, question, choices, correctAnswer) values ('Moon', "What is the orbital period of the moon?", "a, b, c, d", 1);
-INSERT INTO questions (category, question, choices, correctAnswer) values ('Constellation', 'What star will lead you north?', "a, b, c, d", 2);
-INSERT INTO questions (category, question, choices, correctAnswer) values ('Constellation', 'How many stars make up the belt of Orion?', "a, b, c, d", 3);
-INSERT INTO questions (category, question, choices, correctAnswer) values ('Mars', 'When did the first robot land on Mars?', "a, b, c, d", 0);
-INSERT INTO questions (category, question, choices, correctAnswer) values ('Mars', 'What color is Mars?', "a, b, c, d", 1);
+describe Questions;
+
+INSERT INTO questions (id, category, question, choices, correctAnswer, image, createdAt, updatedAt) values (1, 'Moon', "How are far away is the moon?", "238,900 miles, 310,000 miles, 19,586 miles, 524,356 miles", 0, "assets/images/moonRotation.gif", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO questions (id, category, question, choices, correctAnswer, image, createdAt, updatedAt) values (2, 'Moon', "What is the orbital period of the moon?", "2 months, 27 days, 2 weeks, none", 1, "assets/images/moonOrbit.gif", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO questions (id, category, question, choices, correctAnswer, image, createdAt, updatedAt) values (3, 'Constellation', 'What star will lead you north?', "Santa Claus, Celine Dion, Polaris, Big Dipper", 2, "assets/images/polaris.gif", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO questions (id, category, question, choices, correctAnswer, image, createdAt, updatedAt) values (4, 'Constellation', 'How many stars make up the belt of Orion?', "7, 5, 19, 3", 3, "https://thumbs.gfycat.com/IllinformedShockedBlesbok-size_restricted.gif",  CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO questions (id, category, question, choices, correctAnswer, image, createdAt, updatedAt) values (5, 'Mars', 'When did the first robot land on Mars?', "1997, 1900, 2000, 2002", 0, "assets/images/sojourner.gif", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO questions (id, category, question, choices, correctAnswer, image, createdAt, updatedAt) values (6, 'Mars', 'What color is Mars?', "Blue, Red, Green, Hazy", 1, "https://media.giphy.com/media/cmzkzpxPS3xl8hRVJm/giphy.gif", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 SELECT * FROM user;
 SELECT * FROM questions;
